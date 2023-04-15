bcd9525b0e7684d102afe31c23645bdde1d2fabd
Dutu2019 <erictanase2007@gmail.com>
2023-04-14 20:04:07 -0400

    Created Sidebar and Main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Map from "./components/Map/Map";
 
 function App() {
   return (
     <div className="App">
-
+      <div className="main">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="map">
+          <Map />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <div className="title">Absurdistan</div>
+    </div>
+  );
+}
