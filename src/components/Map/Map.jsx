681590c5a45d350576ba9edb6df873e9d7ca8914
Dutu2019 <erictanase2007@gmail.com>
2023-04-16 08:33:01 -0400

    Added map image

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Map.css";
+import map from "../../img/map.jpg";
 
 export default function Map() {
-  return <div>Map</div>;
+  return (
+    <div className="Map">
+      <section className="mapImg">
+        <img src={map} alt="World Map" />
+      </section>
+    </div>
+  );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,17 @@ export default function Sidebar() {
   return (
     <div className="Sidebar">
       <div className="title">Absurdistan</div>
+      <div className="sections">
+        <div className="section">Introduction</div>
+        <div className="section">Endroit 1</div>
+        <div className="section">Endroit 2</div>
+        <div className="section">Eugene Ionesco</div>
+        <div className="section">Boris Vian</div>
+        <div className="section">Albert Camus</div>
+        <div className="section">Conseil 1</div>
+        <div className="section">Conseil 2</div>
+        <div className="section">Conseil 3</div>
+      </div>
     </div>
   );
 }
